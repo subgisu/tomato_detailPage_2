e7eebc74d7fd7520a9fa0ebe5fcde06dbaf96ae5
subgisu <annjin4@gmail.com>
2026-03-03 23:24:06 +0900

    Update routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,9 +1,8 @@
-import { createBrowserRouter } from "react-router";
-import { TomatoDetail } from "./pages/TomatoDetail";
-
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: TomatoDetail,
   },
-]);
+], {
+  basename: "/tomato_detailPage_2",
+});
